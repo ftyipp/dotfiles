5047a5ae3713deb007300979920c54513e45ba26
ftyipp <frtyipp@gmail.com>
2016-01-15 11:59:24 +0100

    wacom: fix rotation

--- wacom.sh
+++ wacom.sh
@@ -3,4 +3,4 @@ device="Wacom Bamboo Connect"
 stylus="$device Pen stylus"
 xsetwacom --set "$stylus" Area 4500 2250 12882 6766
 xinput set-prop "$stylus" --type=float "Coordinate Transformation Matrix" 0.5 0 0 0 1 0 0 0 1
-xsetwacom --set "$stylus" Rotate $1
+xsetwacom --set "$stylus" Rotate half
